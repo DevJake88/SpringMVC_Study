ecf4e9bd8ae8c42947105b29f98a4423c71b4b07
DevJake88 <developer.jake88@gmail.com>
2023-04-11 16:30:51 +0900

    Add rest controller

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,21 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.SampleVO;
+import org.zerock.domain.Ticket;
 
 import lombok.extern.log4j.Log4j;
 
@@ -35,4 +42,47 @@ public class SampleController {
 		return new SampleVO(113, "로켓", "라군");
 	}
 	
+	@GetMapping(value = "/getList")
+	public List<SampleVO> getList() {
+//		return IntStream.range(1,10).mapToObj(i -> new SampleVO(i, i+ "First", i + " Last")).collect(Collectors.toList());
+		ArrayList<SampleVO> list = new ArrayList<SampleVO>();
+		for(int i=1; i<=10; i++) {
+			list.add(new SampleVO(i, i+"First", i+" Last"));
+		}
+		return list;
+	}
+	
+	@GetMapping(value = "/getMap")
+	public Map<String, SampleVO> getMap() {
+		Map<String, SampleVO> map = new HashMap<String, SampleVO>();
+		map.put("First", new SampleVO(111, "그루트", "주니어"));
+		return map;
+	}
+	
+	@GetMapping(value= "/check", params = {"height", "weight"})
+	public ResponseEntity<SampleVO> check(Double height, Double weight) {
+		SampleVO vo = new SampleVO(0, "" + height, "" + weight);
+		ResponseEntity<SampleVO> result = null;
+		
+		if (height < 150) {
+			result = ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
+		} else {
+			result = ResponseEntity.status(HttpStatus.OK).body(vo);
+		}
+		return result;
+	}
+	
+	@GetMapping("/product/{cat}/{pid}")
+	public String[] getPath(
+			@PathVariable("cat") String cat,
+			@PathVariable("pid") Integer pid
+			) {
+		return new String[] {"category: " + cat, "productid: " + pid};
+	}
+	
+	@PostMapping("/ticket")
+	public Ticket convert(@RequestBody Ticket ticket) {
+		log.info("convert....ticket" + ticket);
+		return ticket;
+	}
 }
